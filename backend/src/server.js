7c4d86ad8ea215f9f4c9c5d17fe7581c80977ffe
amanbg451 <rohitamanrazz@gmail.com>
2024-04-11 18:09:56 +0530

    fin

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-// import {fileURLToPath} from 'url';
+import {fileURLToPath} from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js'
@@ -8,11 +8,11 @@ import userRouter from './routers/user.router.js'
 import orderRouter from './routers/order.router.js'
 import uploadRouter from './routers/order.router.js'
 import { dbconnect } from './config/database.config.js';
-// import path, { dirname } from 'path';
+import path, { dirname } from 'path';
 dbconnect();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(
@@ -27,13 +27,13 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/upload', uploadRouter);
 
-// const publicFolder = path.join(__dirname, 'public');
-// app.use(express.static(publicFolder));
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
 
-// app.get('*', (req, res) => {
-//   const indexFilePath = path.join(publicFolder, 'index.html');
-//   res.sendFile(indexFilePath);
-// });
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
 
 const PORT =5000;
 app.listen(PORT, () => {
